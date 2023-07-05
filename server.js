dc1db47b2b1f9dbec010665ba498cd10196a54e4
radupadurariu <padurariuionutradu@gmail.com>
2023-07-05 19:50:32 +0300

    add body parser and try fix heroku

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json())
 app.use(express.static('./'))
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 const PORT = 4000;
 
 app.post('/html/apps/07_weather-app/weather', (req, res) => {
