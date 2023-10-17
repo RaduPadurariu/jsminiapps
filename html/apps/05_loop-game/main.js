a971bacdae215dd3a9b36d75a22aa424a5889e06
radupadurariu <padurariuionutradu@gmail.com>
2023-10-17 20:21:41 +0300

    prevent scrolling when playing in race game

--- html/apps/05_loop-game/main.js
+++ html/apps/05_loop-game/main.js
@@ -279,3 +279,12 @@ function playAgainBtn () {
   replayBtnElem.appendChild(btnPlayAgainElem);
   btnPlayAgainElem.innerText = "Play Again";
 }
+
+// Prevent scrolling page when playing
+
+window.addEventListener("keydown", function(e) {
+  // space and arrow keys
+  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+  }
+}, false);
